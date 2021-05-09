ff47ae4028099e48c34a9c4aa84ebec06987270a
Antonio Calabretta <amc@betsystem.io>
2021-05-09 17:10:32 +0200

    cleaning up comments

--- main.js
+++ main.js
@@ -1,16 +1,14 @@
 'use strict'
-const { Worker, isMainThread, workerData, BroadcastChannel} = require('worker_threads');
-
+const { Worker, isMainThread, workerData, BroadcastChannel } = require('worker_threads');
 const CoinbasePro = require('coinbase-pro');
-//const sleep = require('sleep');
 const { wsUrl } = require('./model/constants');
 const { strategies } = require('./strategies/all_strategies');
 
-//const client = new CoinbasePro.AuthenticatedClient(
-//  process.env.apiKey,
-//  process.env.apiSecret,
-//  process.env.apiPassphrase
-//);
+const client = new CoinbasePro.AuthenticatedClient(
+  process.env.apiKey,
+  process.env.apiSecret,
+  process.env.apiPassphrase
+);
 
 const tickerChannel = new BroadcastChannel('ticker');
 
@@ -32,29 +30,12 @@ if (isMainThread) {
   );
   websocket.on('message', data => {
     tickerChannel.postMessage(data);
-      //console.log(`Type: ${data.type}`);
-      //console.log(` getting stuff:${JSON.stringify(data)}`);
   });
 } else {//worker part
     console.log(`Worker instantiated for ${strategies[workerData.strategy].type()}`);
     tickerChannel.onmessage = (event) => {
       console.log(`[Worker: ticker]`)
+      strategies[workerData.strategy].ticker();
+      strategies[workerData.strategy].action();
     }
-    //parentPort.postMessage('This is Worker');
 }
-
-/*
-if (typeof(someObject.quack) == "function")
-{
-    // This thing can quack
-}
-*/
-
-/**
- * 
- * process.argv.forEach(function (val, index, array) {
-    console.log(index + ': ' + val);
-  });
- */
-
-//https://github.com/ttezel/nn (try with various inputs)

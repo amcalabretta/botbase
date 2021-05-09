ce3f354b449fb20c10a1515bd77183be704664be
Antonio Calabretta <amc@betsystem.io>
2021-05-09 19:28:49 +0200

    fix

--- strategies/candlesticks_kicker_pattern.js
+++ strategies/candlesticks_kicker_pattern.js
@@ -10,28 +10,40 @@
 */
 const cs = require('candlestick');
 const moment = require('moment');
-const OrderType = require('../model/constants').OrderType
+const {OrderType} = require('../model/constants');
+const {isBullishKicker,isBearishKicker} = require('candlestick');
 
 class KickerPatternCandleStickStrategy {
 
     constructor(conf) {
         this.candles=[];
         this.values=[];
-        this.tickMs = conf.tickMs;
-        this.min = 0;
-        this.max = Number.MAX_SAFE_INTEGER;
+        this.tickInSeconds = conf.tickInSeconds;
+        this.min = Number.MIN_SAFE_INTEGER;
+        this.max = 0;
     }
     
     ticker(value){
        var currentTs = moment().unix();  
+       if (this.values.length > 0) {
+        console.log(`${currentTs - this.values[0].ts} - ${currentTs}`);
+       }
        let res = {orderType:OrderType.NO_OP};
-       this.values.push({ts:moment().unix(),price:parseFloat(value)});   
-       if (this.values.length>0 && currentTs - this.values[0].ts>this.tickMs) {
-          this.candles.push({
-                open: this.values[0],
+       this.values.push({ts:currentTs,price:parseFloat(value)}); 
+       if (parseFloat(value) > this.max) {
+           this.max = parseFloat(value);
+       }
+       if (parseFloat(value) < this.min) {
+         this.min = parseFloat(value);
+       }
+
+       if (this.values.length>0 && currentTs - this.values[0].ts>this.tickInSeconds) {
+         console.log('candles!');
+         this.candles.push({
+                open: this.values[0].price,
                 high: this.max,
                 low: this.min,
-                close: this.values[this.values.length-1]
+                close: this.values[this.values.length-1].price
               });
           if (this.candles.length==2) {//we prepare the action
             if (isBullishKicker(this.candles[0], this.candles[1])) {
@@ -45,10 +57,14 @@ class KickerPatternCandleStickStrategy {
           this.values = [];
           this.min = 0;
           this.max = Number.MAX_SAFE_INTEGER;
-       }
-       this.candles.forEach(el=>{
-           console.log(el.high);
-       })
+          this.candles.forEach(el=>{
+             console.log(`H:${el.high}`);
+             console.log(`L:${el.low}`);
+             console.log(`O:${el.open}`);
+             console.log(`C:${el.close}`);
+             console.log('===========')
+          })
+        }
        return res
     }
     

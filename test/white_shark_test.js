03ec9152cf83903a9f9198df90c97449ae20f1f6
Antonio M. Calabretta <antonio.calabretta@ing.com>
2022-02-09 10:06:42 +0100

    fixes

--- test/white_shark_test.js
+++ test/white_shark_test.js
@@ -7,6 +7,9 @@ const { expect } = require('chai');
 const sinon = require('sinon');
 const log4js = require('log4js');
 const { WhiteShark } = require('../strategies/candlesticks/white_shark');
+const { OrderType } = require('../model/constants');
+const { Order } = require('../model/order');
+
 
 const strategy = new WhiteShark({
   markets: ['LTC-EUR'], cryptoAmounts: [10], euroAmount: 30, dollarAmount: 0
@@ -31,9 +34,22 @@ var firstArgument = myStub.getCall(0).args[0];
 assert.equal(firstArgument, expectedValue);
  */
 
-describe('White Shark Bullish Detection', () => {
-  it('Should trigger a bullish order', (done) => {
+describe('White Shark Ticker response', () => {
+  it('Should trigger a NO_OP when a ticker is send', (done) => {
     strategy.valueCallBack({ type: 'ticker', price: 107 });
+    sinon.assert.calledWith(stub, new Order(OrderType.NO_OP, 0, 0, 0, 0));
     done();
   }).timeout(60000);
 });
+
+/*describe('White Shark Bullish reponse', () => {
+  it('Should detect a bullish pattern', (done) => {
+    strategy.valueCallBack({
+      type:'candlesPastTenMinutes',
+      payload:[]
+    });
+    sinon.assert.calledWith(stub, new Order(OrderType.NO_OP, 0, 0, 0, 0));
+    done();
+  }).timeout(60000);
+});*/
+

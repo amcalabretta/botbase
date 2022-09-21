99e847890fed1d25b23981a1026c6d01a11fddea
Antonio M. Calabretta <antonio.calabretta@ing.com>
2022-09-21 12:16:51 +0200

    splitting the complexity in two function

--- utils/loadConfigurationFile.js
+++ utils/loadConfigurationFile.js
@@ -6,16 +6,23 @@
 const fs = require('fs');
 const yaml = require('js-yaml');
 
+const checkArgs = (argv) => {
+    if (argv.length !== 3) {
+      throw new Error('Usage: node main --conf=/path/to/yaml/file');
+    }
+}
+
 const loadConfigurationFile = (argv) => {
   try {
-    if (argv.length === 3) {
-      const confParam = argv[2];
-      const paramTokens = confParam.split('=');
-      if (paramTokens.length === 2 && paramTokens[0] === '--conf') {
-        return yaml.load(fs.readFileSync(paramTokens[1], 'utf8'));
-      }
+    checkArgs(argv);
+    const confParam = argv[2];
+    const paramTokens = confParam.split('=');
+    if (paramTokens.length === 2 && paramTokens[0] === '--conf') {
+      const data = yaml.load(fs.readFileSync(paramTokens[1], 'utf8'));
+      if (!data.logging.logDir) throw new Error(`Logging directory not found in file ${paramTokens[1]}`);
+      if (!fs.existsSync(data.logging.logDir)) throw new Error(`Logging directory  ${data.logging.logDir} does not exists`);
+      return data;
     }
-    throw new Error('Usage: node main --conf=/path/to/yaml/file');
   } catch (e) {
     throw new Error(`${e}`);
   }

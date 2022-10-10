beda8d8c648c6d90025a5629434e1fc5aaf60652
Antonio Calabretta <amc@betsystem.io>
2022-10-10 07:37:15 +0200

    linting

--- utils/getCandles.js
+++ utils/getCandles.js
@@ -1,7 +1,7 @@
 const moment = require('moment');
 const Table = require('easy-table');
 /**
- * Function getting the candles from coinbase for a set of
+ * Function getting the candles from coinbase for a set of markets
  */
 const getCandles = async (client, logger, markets, granularity, numMinutes, channel) => {
   const time = await client.rest.time.getTime();
@@ -10,16 +10,16 @@ const getCandles = async (client, logger, markets, granularity, numMinutes, chan
   logger.info(`Current: ${currentTimeStamp.utc().format('YYYY-MM-DDTHH:mm:00.000Z')}`);
   logger.info(`Previous: ${previousTimeStamp.utc().format('YYYY-MM-DDTHH:mm:00.000Z')}`);
   const allCandles = [];
-  for (const mkt in markets) {
-    logger.info(`Market:${markets[mkt]}`);
+  markets.forEach((mkt, i) => {
+    logger.info(`${i + 1}/${markets.length} - :${mkt}`);
     try {
       allCandles[mkt] = client.rest.product.getCandles(markets[mkt], {
-        end:currentTimeStamp.utc().format('YYYY-MM-DDTHH:mm:00.000Z'),
+        end: currentTimeStamp.utc().format('YYYY-MM-DDTHH:mm:00.000Z'),
         granularity,
         start: previousTimeStamp.utc().format('YYYY-MM-DDTHH:mm:00.000Z')
       });
       const t = new Table();
-      candles.forEach((c) => {
+      allCandles.forEach((c) => {
         t.cell('TS:', c.openTimeInMillis);
         t.cell('Low', c.low, Table.number(3));
         t.cell('High', c.high, Table.number(3));
@@ -34,7 +34,7 @@ const getCandles = async (client, logger, markets, granularity, numMinutes, chan
     } catch (error) {
       logger.error('Error');
     }
-  }
+  });
   await Promise.all(allCandles);
 };
 

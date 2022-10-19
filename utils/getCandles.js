637e9a771dfea9a1151dcd8fe3961f0c133e53c8
Antonio M. Calabretta <antonio.calabretta@ing.com>
2022-10-19 09:03:07 +0200

    bugfix:getting candles and logging them as a table for better reading

--- utils/getCandles.js
+++ utils/getCandles.js
@@ -1,16 +1,18 @@
 const moment = require('moment');
 const Table = require('easy-table');
 
-const serializeCandle = (candle) => {
+const serializeCandles = (candles) => {
   const t = new Table();
-  t.cell('TS:', c.openTimeInMillis);
-  t.cell('Low', c.low, Table.number(3));
-  t.cell('High', c.high, Table.number(3));
-  t.cell('Open', c.open, Table.number(3));
-  t.cell('Close', c.close, Table.number(3));
-  t.cell('Volume', c.volume, Table.number(3));
-  t.cell('Raw TS', c.openTimeInISO);
-  t.newRow();
+  candles.forEach((c) => {
+    t.cell('TS:', c.openTimeInMillis);
+    t.cell('Low', c.low, Table.number(3));
+    t.cell('High', c.high, Table.number(3));
+    t.cell('Open', c.open, Table.number(3));
+    t.cell('Close', c.close, Table.number(3));
+    t.cell('Volume', c.volume, Table.number(3));
+    t.cell('Raw TS', c.openTimeInISO);
+    t.newRow();
+  })
   return t.toString();
 }
 
@@ -25,16 +27,13 @@ const getCandles = async (client, logger, markets, granularity, numMinutes, chan
   logger.info(`Previous: ${previousTimeStamp.utc().format('YYYY-MM-DDTHH:mm:00.000Z')}`);
   const allCandles = [];
   markets.forEach((mkt, i) => {
-    logger.info(`${i + 1}/${markets.length} - :${mkt}`);
-    allCandles[mkt] = client.rest.product.getCandles(markets[mkt], {
+    allCandles[mkt] = client.rest.product.getCandles(mkt, {
       end: currentTimeStamp.utc().format('YYYY-MM-DDTHH:mm:00.000Z'),
       granularity,
       start: previousTimeStamp.utc().format('YYYY-MM-DDTHH:mm:00.000Z')
     }).then((candles) => {
-      candles.forEach((c) => {
-        logger.info(`${markets[mkt]} - \n ${serializeCandle(c)}`);
-        channel.postMessage({ type: 'candlesPastTenMinutes', market: markets[mkt], payload: {} });
-      })
+      logger.info(`${i + 1}/${markets.length} - :${mkt} \n ${serializeCandles(candles)}`);
+      channel.postMessage({ type: 'candlesPastTenMinutes', market: mkt, payload: {} });
     }).catch(error => logger.error(`Error:${error}`));
   });
   await Promise.all(allCandles);

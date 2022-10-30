86f520c71d18276e6424b4b7c36c798692983fe0
Antonio M. Calabretta <antonio.calabretta@ing.com>
2022-10-30 11:19:58 +0100

    adding git dir to the excluded patterns for todo

--- scripts/todo.sh
+++ scripts/todo.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 echo "Files having TODOs and FIXMEs:"
-files=$(grep -ro -n 'TODO\|FIXME' *.js . --exclude-dir={node_modules,test,scripts})
+files=$(grep -ro -n 'TODO\|FIXME' *.js . --exclude-dir={node_modules,test,scripts,.git})
 for FILE in $files; do echo $FILE; done
